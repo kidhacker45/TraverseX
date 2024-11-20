204dc7582cecd58eb5012e75a814bbc9ea775c3c
KIDHACKER <129326203+kidhacker45@users.noreply.github.com>
2024-11-20 16:49:37 +0530

    logofile.py

--- logofile.py
+++ logofile.py
@@ -1,9 +1,19 @@
+from colorama import Fore, Style, init  # For color-coded output
+
+# Initialize colorama
+init(autoreset=True)
+
+# Define custom colors
+NEON_YELLOW_GREEN = "\033[1;35m"  # Bold purple
+RESET = "\033[0m"
+
 def print_logo():
-    print(r"""
+    print(
+    f"{NEON_YELLOW_GREEN}" + r"""
  _____                                 __  __
 |_   _| __ __ ___   _____ _ __ ___  ___\ \/ /
   | || '__/ _` \ \ / / _ \ '__/ __|/ _ \\  / 
   | || | | (_| |\ V /  __/ |  \__ \  __//  \ 
   |_||_|  \__,_| \_/ \___|_|  |___/\___/_/\_\ by KIDHACKER45
-
-    """)
+""" + RESET
+)
